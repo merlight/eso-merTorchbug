ae2cd186f66fa3e23c2b626bb92dc9ea2bebb896
Baertram <pz1@gmx.net>
2023-01-21 17:56:02 +0100

    [merTorchbug Fixed an improved - v1.62] -Added slash command /tbst or /tbugst to show the strings -Added "slash commands with additional search term" --e.g. /tbm bagId to open the control below the mouse and search at the inspector for bagId --or /tbst test to open the global inspector "Strings" tab and search for "test" -Added /tb <number> to open the global inspector tab with that index directly (1=addons, 2=classes, ...) -Added tooltip at "mouse over" created inspector tabs (which only show numbers), showing the name of the inspected control/variable -Added right mouse button context menu to the expand&collapse, refresh and update button at the inspector windows -Added right mouse button context menu to the # label at the bottom right of the inspector window -Changed context menu at the title icon (top left) to only show with a right mouse button click -Double clicking the title icon (top left) will change the current inspector windows draw layer between normal and onTop -Changed "Set draw layer" context menu entries to hide the current draw layer -Changed selected tab title, at an inspector, to show the complete tab's name now, and not the abbreviated number/text -Added some more local reference variables for the performance

--- modules/constants.lua
+++ modules/constants.lua
@@ -2,7 +2,7 @@ TBUG = {}
 local tbug = TBUG or SYSTEMS:GetSystem("merTorchbug")
 
 --Version and name of the AddOn
-tbug.version =  "1.63"
+tbug.version =  "1.62"
 tbug.name =     "merTorchbug"
 tbug.author =   "merlight, current: Baertram"
 
